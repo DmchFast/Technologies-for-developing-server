5db1ffcb8b79b60c9af17949c55359625fa7dcce
DmchFast <chdima28@gmail.com>
2026-02-25 01:33:22 +0300

    Adding GET html

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication_ASP.NET_Core.Controllers
+namespace WebApplication_ASP.NET_Core.Controllers;
+
+[Route("/")]
+[ApiController]
+public class ValuesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+    [HttpGet]
+    public object Start()
     {
+        return new { message = "Добро пожаловать в моё приложение ASP.NET Core!" };
+    }
+
+    [HttpGet("html")]
+    public IActionResult Html()
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "page", "index.html");
+        return PhysicalFile(filePath, "text/html");
     }
 }
